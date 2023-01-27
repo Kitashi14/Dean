9039457caa6083ea7db74eb9da744180f47d3c76
Kitashi14 <rishavrajaryan14@gmail.com>
2023-01-28 05:13:40 +0530

    current status added

--- pages/admin.php
+++ pages/admin.php
@@ -24,11 +24,19 @@ if (isset($_SESSION['uid']) && isset($_SESSION['isAdmin'])) {
             <h2> Name : <?php echo $_SESSION['name'] ?></h2>
             <h2> Email : <?php echo $_SESSION['email'] ?></h2>
             <h2> Category : <?php echo $_SESSION['category'] ?></h2>
+
         </div>
 
-        <!-- button linked to set status form page  -->
-        <div class=" w-1/2 mt-4 bg-gray-200 py-10 px-4 flex flex-row justify-evenly space-x-2 items-center">
-            <a class="bg-green-600 h-10 text-center flex items-center text-white py-1 px-3 ml-5" href="<?php echo rootUrl, '/pages/statusForm.php'; ?>">Set Status</a>
+        <!--current status and  button to set status form page  -->
+        <div class=" w-1/2 mt-4 bg-gray-200 py-10 px-4 flex flex-col justify-evenly items-center">
+
+            <div class="bg-gray-200 mt-1 w-1/2 flex flex-col justify-around items-center ">
+                <h2 class="block mb-2 text-m font-bold text-blue-600"> Current Semester : <?php echo $_SESSION['currentSemester'] ?></h2>
+                <h2 class="block mb-2 text-m font-bold text-<?php echo $_SESSION['isCourseEntryAllowed']? 'green': 'red'; ?>-600" > <?php echo $_SESSION['isCourseEntryAllowed']? 'Course entry allowed': 'Course entry blocked'; ?></h2>
+                <h2 class="block mb-2 text-m font-bold text-<?php echo $_SESSION['isGradeEntryAllowed']? 'green': 'red'; ?>-600" > <?php echo $_SESSION['isGradeEntryAllowed']? 'Grade entry allowed': 'Grade entry blocked'; ?></h2>
+
+            </div>
+            <a class="bg-green-600 h-10 text-center flex items-center text-white py-1 px-3 " href="<?php echo rootUrl, '/pages/statusForm.php'; ?>">Change Status</a>
 
         </div>
     </div>
