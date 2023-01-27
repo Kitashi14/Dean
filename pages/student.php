43e12b9b9fb32c507523f860c6ef60a0661f6819
Kitashi14 <rishavrajaryan14@gmail.com>
2023-01-28 01:50:44 +0530

    removed debugging prints

--- pages/student.php
+++ pages/student.php
@@ -18,20 +18,7 @@ if (isset($_SESSION['uid']) && isset($_SESSION['category'])) {
 
 This is student page
 <br>
-<?php
-echo $_SESSION['uid'];
-echo '<br>';
-echo $_SESSION['category'];
-echo '<br>';
-echo $_SESSION['isAdmin'];
-echo '<br>';
-echo $_SESSION['regNo'];
-echo '<br>';
-echo $_SESSION['name'];
-echo '<br>';
-echo $_SESSION['program'];
-echo '<br>';
-?>
+
 <?php
 include './../inc/footer.php';
 ?>
