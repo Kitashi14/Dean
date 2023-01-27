f9d0ba21a5f18acae5957ddb585b5c67e4b6e56a
Kitashi14 <rishavrajaryan14@gmail.com>
2023-01-27 21:11:17 +0530

    db conn success displayed on console

--- database.php
+++ database.php
@@ -10,6 +10,9 @@ $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 // Check connection
 if ($conn->connect_error) {
   die('Connection failed: ' . $conn->connect_error);
+} else{
+    echo '<script>console.log("connected to database")</script>';
 }
 
-// echo 'Connected successfully';
+?>
+
