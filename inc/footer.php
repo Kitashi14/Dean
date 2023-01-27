7694736ed6484d6d692ebc36340ce139036f8ccd
Kitashi14 <rishavrajaryan14@gmail.com>
2023-01-28 01:50:18 +0530

    minor correction

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <footer class="fixed left-0 bottom-0 w-full text-center p-1 bg-red-600 text-white">
-    MNNIT Web Team Round 2 Project
+    MNNIT Dean Web Team Round 2 Project
 </footer>
 </body>
 
