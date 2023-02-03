17a2ea8ba4f3cd293d871125c16bb16c60c4bcdb
Kitashi14 <rishavrajaryan14@gmail.com>
2023-02-03 14:42:09 +0530

    deleted file

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//storing env variables
-define('rootUrl', 'http://localhost/deanproject');
-define('DB_HOST', 'localhost');
-define('DB_USER', 'kitashi');
-define('DB_PASS', 'kitashi');
-define('DB_NAME', 'dean_project');
